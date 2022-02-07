d761a02c4e607ec67f6150119a5c2e0eec5993eb
ishandushettiwar7x <ishan.dushettiwar@sevenx.io>
2022-02-07 13:19:51 +0530

    Second Commit

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -1,68 +1,66 @@
 
 public class SpiralMatrix {
 	
-	public static void traverse(int[][] matrix,int iteration,int n,int row,
-			int coloumn,int dir) {
-		if (iteration == n*n+1) {
+	public static final int RIGHT = 1;
+	public static final int DOWN = 2;
+	public static final int LEFT = 3;
+	public static final int UP = 0;
+	
+	
+	public static void traverse(int[][] matrix, int currentNumber, int input, 
+			int row, int column, int dir) {
+		if (currentNumber == input * input + 1) {
 			return;
-			}else if ((row == 0)&&(coloumn == n-1) || (row == n-1)&&(coloumn == n-1) ||
-				((row == n-1)&&(coloumn == 0))){
-			dir++;
-		}
-		if (dir%4 == 1) {
-			matrix[row][coloumn] = iteration;
-			if (coloumn == n-1) {
-				traverse(matrix,iteration+1,n,row+1,coloumn,dir);
-			}else if(matrix[row][coloumn+1] != 0) {
+			} 
+		
+		matrix[row][column] = currentNumber;
+		currentNumber++;
+		
+		if (dir % 4 == RIGHT) {
+			if (((row == 0) && (column == input-1)) || (matrix[row][column + 1] != 0)) {
 				dir++;
-				traverse(matrix,iteration+1,n,row+1,coloumn,dir);
-			}else {
-				traverse(matrix,iteration+1,n,row,coloumn+1,dir);
+				row++;
+			} else {
+				column++;
 			}
-		}else if (dir%4 == 2) {
-			matrix[row][coloumn] = iteration;
-			if (row == n-1) {
-				traverse(matrix,iteration+1,n,row,coloumn-1,dir);
-			}else if(matrix[row+1][coloumn] != 0) {
+		} else if (dir % 4 == DOWN) {
+			if (((row == input-1) && (column == input-1)) || (matrix[row + 1][column] != 0)) {
 				dir++;
-				traverse(matrix,iteration+1,n,row,coloumn-1,dir);
-			}else {
-				traverse(matrix,iteration+1,n,row+1,coloumn,dir);
+				column--;
+			} else {
+				row++;
 			}
-		}else if (dir%4 == 3) {
-			matrix[row][coloumn] = iteration;
-			if (coloumn == 0) {
-				traverse(matrix,iteration+1,n,row-1,coloumn,dir);
-			}else if(matrix[row][coloumn-1] != 0) {
+		} else if (dir % 4 == LEFT) {
+			if (((row == input-1) && (column == 0)) || (matrix[row][column - 1] != 0)) {
 				dir++;
-				traverse(matrix,iteration+1,n,row-1,coloumn,dir);
-			}else {
-				traverse(matrix,iteration+1,n,row,coloumn-1,dir);
+				row--;
+			} else {
+				column--;
 			}
-		}else if (dir%4 == 0) {
-			matrix[row][coloumn] = iteration;
-			if(matrix[row-1][coloumn] != 0) {
+		} else if (dir % 4 == UP) {
+			if (matrix[row - 1][column] != 0) {
 				dir++;
-				traverse(matrix,iteration+1,n,row,coloumn+1,dir);
-			}else {
-				traverse(matrix,iteration+1,n,row-1,coloumn,dir);
+				column++;
+			} else {
+				row--;
 			}
 			
 		}
+		traverse(matrix,currentNumber,input,row,column,dir);
 		
 	}
 	
-	public static void Spiral(int n) {
-		int[][] matrix = new int[n][n];
-		int iteration = 1, row = 0,coloumn = 0,dir = 1;
+	public static void spiral(int input) {
+		int[][] matrix = new int[input][input];
+		int currentNumber = 1, row = 0,column = 0,dir = RIGHT;
 		// Let dir be direction variable where its modulus of 4 will be calculated, where 1 means right,2 means down,3 means left and 0 means up 
-		traverse(matrix,iteration,n,row,coloumn,dir);
-		for (int i = 0;i < n;i++) {
-			for (int j = 0; j < n; j++) {
+		traverse(matrix,currentNumber,input,row,column,dir);
+		for (int i = 0;i < input;i++) {
+			for (int j = 0; j < input; j++) {
 				if (matrix[i][j] < 10) {
-					System.out.print(" "+matrix[i][j]+" ");
+					System.out.print(" " + matrix[i][j] + " ");
 				}else {
-					System.out.print(matrix[i][j]+" ");
+					System.out.print(matrix[i][j] + " ");
 				}
 			}
 			System.out.println("");
@@ -71,7 +69,7 @@ public class SpiralMatrix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Spiral(9);
+		spiral(4);
 
 	}
 
